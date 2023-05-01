c97c283e7bdb25229008d1083fab51cbdc7f84ff
Fortune <Fortune971114@protonmail.com>
2023-05-01 19:47:23 +0200

    Added Jinja2 Templates Inheritance

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask 
+from flask import Flask, render_template 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Fortune971114',
+        'title': 'Post 1',
+        'content': 'This is my first conent',
+        'date_posted': 'May, 01, 2023'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Post 2',
+        'content': '2nd conent on this sire',
+        'date_posted': 'May, 02, 2023'
+    }
+
+    ]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h2>Flask Home Page</h2>";
+    return render_template("home.html", posts=posts)
     
 @app.route("/about")
 def about():
-        return "<h2>The About Page</h2>";
+        return render_template("about.html", title="About")
